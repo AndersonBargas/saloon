7dfed1856ed30fe06e558a7d19a30f5ebb8c3026
Anderson Bargas <bargas.anderson@gmail.com>
2018-05-18 21:14:33 -0300

    Adicionando CRUD de Usuários.

---
+++ views/usuarios/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Usuarios */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="usuarios-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'senha')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'administrador')->checkbox() ?>
+
+    <?= $form->field($model, 'dataCriacao')->textInput() ?>
+
+    <?= $form->field($model, 'dataExclusao')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/usuarios/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\UsuariosSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Usuarios';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="usuarios-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Usuarios', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'nome',
+            'email:email',
+            'senha',
+            'administrador:boolean',
+            //'dataCriacao',
+            //'dataExclusao',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
+</div>
