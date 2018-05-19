aacaa2e979a34b4ce4629ae8bdf62ce8c5c5146c
Anderson Bargas <bargas.anderson@gmail.com>
2018-05-19 16:05:38 -0300

    Redirecionando para a lista de usuários após a inclusão.

--- controllers/UsuariosController.php
+++ controllers/UsuariosController.php
@@ -67,7 +67,7 @@ class UsuariosController extends Controller
         $model = new Usuarios();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('create', [
