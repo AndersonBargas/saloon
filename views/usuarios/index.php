4dbc5f4d9c96c5e8ee34b4dffb655b04c62f35d9
Anderson Bargas <bargas.anderson@gmail.com>
2018-05-20 20:29:27 -0300

    Melhorando feedback visual dos usuários.

--- views/usuarios/index.php
+++ views/usuarios/index.php
@@ -12,6 +12,12 @@ $this->title = 'Usuários';
 ?>
 <div class="usuarios-index">
 
+    <?php if (Yii::$app->session->hasFlash('sucesso')): ?>
+        <div class="alert alert-success" role="alert">
+            <strong>Excelente!</strong> <?= Yii::$app->session->getFlash('sucesso') ?>
+        </div>
+    <?php endif; ?>
+
     <h1><?= Html::encode($this->title) ?></h1>
     <?php Pjax::begin(); ?>
 
@@ -45,7 +51,7 @@ $this->title = 'Usuários';
                                 },
                                 'usuarioExcluir' => function ($url, $model) {
                                     $url = Url::to(['usuarios/excluir', 'id' => $model->id]);
-                                    return Html::a('<span class="fa fa-trash-alt"></span>', $url, [
+                                    return Html::a('<span class="fa fa-trash-alt text-danger"></span>', $url, [
                                         'title'        => 'Excluir',
                                         'data-confirm' => 'Tem certeza que deseja excluir este usuário?',
                                         'data-method'  => 'post',
