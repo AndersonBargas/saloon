fae38d2319a4b4a113b46b49a0562a9a32942259
Anderson Bargas <bargas.anderson@gmail.com>
2018-05-20 16:57:36 -0300

    Adicionando calendàrio à tela de reservas.

--- views/reservas/index.php
+++ views/reservas/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\web\View;
 
 use app\widgets\Reservas;
 
@@ -23,14 +24,67 @@ $this->title = 'Reservas';
     <h1><?= Html::encode($this->title) ?></h1>
     <?php Pjax::begin(); ?>
 
-    <p>
-        <?= Html::a('Nova Reserva', ['adicionar'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <h1 class="text-center"><a id="data_por_extenso" href="#" onclick="calendarioToggle(event)"></a></h1>
 
-    <?= Reservas::widget() ?>
+    <div class="row">
+        <div class='col-sm-offset-3 col-sm-6'>
+            <div class="form-group">
+                <div class='input-group date' id='datetimepicker2'>
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?= Reservas::widget([
+        'data' => $data,
+    ]) ?>
 
     <p>
         <?= Html::a('<i class="fa fa-plus"></i> Nova Sala', ['salas/adicionar'], ['class' => 'btn btn-primary btn-lg btn-block']) ?>
     </p>
 
 </div>
+<?php
+    View::registerJs(
+    "calendario = $('#datetimepicker2').datetimepicker({
+        date    : '{$data}',
+        format  : 'dddd, DD [de] MMMM [de] YYYY',
+        inline  : true,
+        locale  : 'pt-br',
+        tooltips: {
+            today        : 'Hoje',
+            clear        : 'Limpar',
+            close        : 'Fechar',
+            selectMonth  : 'Selecione o Mês',
+            prevMonth    : 'Mês Anterior',
+            nextMonth    : 'Próximo Mês',
+            selectYear   : 'Selecione o Ano',
+            prevYear     : 'Ano Anterior',
+            nextYear     : 'Próximo Ano',
+            selectDecade : 'Selecione a Década',
+            prevDecade   : 'Década Anterior',
+            nextDecade   : 'Próxima Década',
+            prevCentury  : 'Previous Century',
+            nextCentury  : 'Próximo Século'
+        }
+    });
+
+    var setarData = function(data) {
+        calendario.hide();
+        var formatado = moment(data).format('dddd, DD [de] MMMM [de] YYYY');
+        $('#data_por_extenso').text(formatado);
+    };
+
+    calendario.on('dp.change', function(e){
+        setarData(e.date);
+        var formatado = moment(e.date).format('YYYY-MM-DD');
+        window.location.replace('/reservas/' + formatado);
+    });
+
+    setarData('{$data}');",
+    View::POS_READY
+    );
+?>
