04b3b449eb950cc349a411839fa8dfbaa393330c
Anderson Bargas <bargas.anderson@gmail.com>
2018-05-20 16:59:59 -0300

    Incluindo configurações do calendário.

--- web/js/site.js
+++ web/js/site.js
@@ -1,4 +1,15 @@
+var calendario = null;
+
+function calendarioToggle(ev) {
+    ev.preventDefault();
+    calendario.toggle();
+}
+
+
 jQuery(function ($) {
+
+    moment.locale('pt-br');
+
     var elem = document.querySelectorAll('.js-switch');
     elem.forEach(
         function (el, i) {
@@ -7,29 +18,31 @@ jQuery(function ($) {
     );
     
     yii.allowAction = function ($e) {
-            var message = $e.data('confirm');
-            return message === undefined || yii.confirm(message, $e);
-        };
-        yii.confirm = function (message, ok, cancel) {
-            bootbox.confirm({
-        	    message: message,
-        	    buttons: {
-        	        confirm: {
-                            label: 'Excluir!',
-                            className: 'btn-danger'
-                    },
-                    cancel: {
-                            label: 'Manter como está',
-                            className: 'btn-success'
-                    }
-        		},
-                callback: function (confirmed) {
-                    if (confirmed) {
-                        !ok || ok();
-                    } else {
-                        !cancel || cancel();
-                    }
-        		}
-        	});
-        }
+        var message = $e.data('confirm');
+        return message === undefined || yii.confirm(message, $e);
+    };
+
+    yii.confirm = function (message, ok, cancel) {
+        bootbox.confirm({
+    	    message: message,
+    	    buttons: {
+    	        confirm: {
+                        label: 'Excluir!',
+                        className: 'btn-danger'
+                },
+                cancel: {
+                        label: 'Manter como está',
+                        className: 'btn-success'
+                }
+    		},
+            callback: function (confirmed) {
+                if (confirmed) {
+                    !ok || ok();
+                } else {
+                    !cancel || cancel();
+                }
+    		}
+    	});
+    }
+
 });
