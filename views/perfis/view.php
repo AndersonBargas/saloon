7a15931d9c5635bd8a3b5e76ca741897b623e49b
Anderson Bargas <anderson@andersonbargas.com>
2018-10-13 18:10:40 -0300

    Formatação da coluna de criação do perfil.

--- views/perfis/view.php
+++ views/perfis/view.php
@@ -28,9 +28,9 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
+            //'id',
             'nome',
-            'criacao',
+            'criacao:datetime',
         ],
     ]) ?>
 
