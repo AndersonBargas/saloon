0aa27bfac082a19b290d6ed5e3866841d6b6c054
Anderson Bargas <anderson@andersonbargas.com>
2018-10-13 18:07:55 -0300

    Ajuste no nome do botão de inclusão de novo perfil.

--- views/perfis/index.php
+++ views/perfis/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Perfis', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Novo Perfil', ['adicionar'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'nome',
             'criacao',
 
