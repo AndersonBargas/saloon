8c8d6e7926936ed8fb16f98a558fbc0ee68b70ff
Anderson Bargas <anderson@andersonbargas.com>
2018-10-13 18:07:07 -0300

    Remoção do campo de criação e ajuste no nome do botão salvar/atualizar.

--- views/perfis/_form.php
+++ views/perfis/_form.php
@@ -14,10 +14,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'criacao')->textInput() ?>
-
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    <?= Html::submitButton($model->isNewRecord ? 'Salvar' : 'Atualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
