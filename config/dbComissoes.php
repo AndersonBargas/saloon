7e388de29a3c89825697d39562a14d71be5d3f3c
Marcos Longo <lonmarcos@gmail.com>
2018-10-14 15:55:50 -0300

    SQL Comissoes e ajuste menu

---
+++ config/dbComissoes.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=' . '127.0.0.1:3306' . ';dbname=' . 'comissoes',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'on afterOpen' => function($event) { 
+        $event->sender->createCommand("SET time_zone='-03:00';")->execute(); 
+    },
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
