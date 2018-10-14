8289483c9f22ff740d3081df765816706d0f4753
Marcos Longo <lonmarcos@gmail.com>
2018-10-14 16:01:14 -0300

    SQL Comissoes e ajuste menu

--- config/dbComissoes.php
+++ config/dbComissoes.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=' . '127.0.0.1:3306' . ';dbname=' . 'comissoes',
+    'dsn' => 'mysql:host=' . '127.0.0.1:3306' . ';dbname=' . 'comissao',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
